3615db8eafff4c281c55ef0c51f98574bee275ce
Raylene <raylene.mac@gmail.com>
2017-02-16 17:46:32 -0500

    clean up naive bayes code

--- Raylene's code/naive_bayes.py
+++ Raylene's code/naive_bayes.py
@@ -5,7 +5,6 @@ import numpy
 import nltk
 import re
 import string
-
 input_train = open("train_input.csv")
 data_input = csv.reader(input_train, delimiter=',')
 next(data_input)  # skip header row
@@ -24,16 +23,18 @@ test_output = csv.writer(output_test)
 topics = ['hockey', 'movies', 'nba', 'news', 'nfl', 'politics', 'soccer', 'worldnews']
 tags = re.compile('<.*?>')
 all_input, all_output, unique_words = [], [], []
-class_freq = [0]*8
+class_freq = [0] * 8
 
+#  runs through input data, parses out tags, tokenizes into words
 for r in data_input:
     id = r[0]
     parsed_text = re.sub(tags, '', r[1])
     p = parsed_text.translate(None, string.punctuation)
     words = nltk.word_tokenize(p)
-    unique_words.extend(words)
+    unique_words.extend(words)  # build a set of all unique words in the corpus
     all_input.append(words)
 
+    #  collect all the topic labels in the training set, give integer ID, count frequencies of each label
 for r in data_output:
     id2 = int(r[0])
     label = topics.index(r[1])
@@ -44,18 +45,14 @@ total = sum(class_freq)
 
 unique_words = set(unique_words)
 dicts, all_data = [], []
-for i in range(8):
-    dicts.append({w:0 for w in unique_words})
+for i in range(8):  # build a dictionary of word frequencies for each topic
+    dicts.append({w: 0 for w in unique_words})
 
 for pair in zip(all_input, all_output):
-    d = dicts[pair[1]]  #get word frequency dictionary for current label
+    d = dicts[pair[1]]  # get word frequency dictionary for current label
     for w in pair[0]:
         d[w] += 1
-    #all_data.append(pair)
-#for x in dicts:
-#    print x
-
-
+# compute the probability of the given words occurring in a document of label i
 def compute_probs(words, i):
     p_x_given_y = 1.0
     d_i = dicts[i]
@@ -65,27 +62,22 @@ def compute_probs(words, i):
         if w not in unique_words:  # word doesn't exist in training set
             d_i[w] = 0
         p_x_given_y *= float(d_i[w] + 1) / float(num_i + 2)
-        #print p_x_given_y, d_i[w]
-    #print p_x_given_y, prob_i
-    return p_x_given_y * prob_i
 
+    return p_x_given_y * prob_i
 test_labels = []
 j = 0
-for r in test_input:
-    parsed_text = re.sub(tags, '', r[1])
+for r in test_input:  # here we construct the predictions for the testing input
+    parsed_text = re.sub(tags, '', r[1])  # perform the same pre-processing of the data
     p = parsed_text.translate(None, string.punctuation)
     words = nltk.word_tokenize(p)
     probs = []
-    for i in range(8):
+    for i in range(8):  # compute likelihood of the current sample being of each class
         p_y_given_x = compute_probs(words, i)  # assume label is i, compute P(x_i | y=i) * P(y=i)
         probs.append(p_y_given_x)
-    #print probs
-    label_index = probs.index(max(probs))
-    #print label_index
+    label_index = probs.index(max(probs))  # get the ID of the most likely topic
     test_labels.append([j, topics[label_index]])
     j += 1
 
-#print test_labels[:100]
 test_output.writerows(test_labels)
 
 
